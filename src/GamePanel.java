832d0b3617a1abe131341c5ac2613e6c73913122
dexter24x7 <117462852+shubhjena@users.noreply.github.com>
2023-02-11 11:46:35 +0530

    apple.png added

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -52,13 +52,13 @@ public class GamePanel extends JPanel implements ActionListener {
             for (int i = 0; i < bodyParts; i++) {
                 if (i==0){
                     g.setColor(Color.GREEN);
-                    g.fillOval(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+                    g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
                 }
                 else {
                     g.setColor(new Color(45, 180, 0));
                     //colourful snake
                     g.setColor(new Color(random.nextInt(255), random.nextInt(255),random.nextInt(255)));
-                    g.fillOval(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 }
             }
             g.setColor(Color.YELLOW);
